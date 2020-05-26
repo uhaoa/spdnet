e488d3362d396d689df500f7c957c20e33db6972
xuhao_xuhao <xuhao_xuhao@126.com>
2020-05-26 20:51:14 +0800

    optimize

--- examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xuhao/nettest/spdnet/spdnet/examples
-# Build directory: /home/xuhao/nettest/spdnet/spdnet/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
